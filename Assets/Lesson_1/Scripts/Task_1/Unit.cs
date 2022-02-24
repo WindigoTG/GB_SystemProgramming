861d5d0d7417bdb8fe02413a0f3b605d2582e1b2
Windigo The Green <windigotp@gmail.com>
2022-02-24 15:53:24 +0800

    Lesson_1 Task_1 is done

---
+++ Assets/Lesson_1/Scripts/Task_1/Unit.cs
@@ -0,0 +1,100 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Lesson_1.Task_1
+{
+    public class Unit : MonoBehaviour
+    {
+        #region Fields
+
+        [SerializeField] Slider _healthBar;
+        [SerializeField] Button _startHealingButton;
+        [SerializeField] Button _resetHealthButton;
+        [SerializeField] Text _healthText;
+        [Space]
+        [SerializeField] bool _canRenewHealing;
+
+        private int _health;
+
+        private Coroutine _healing;
+
+        private const int MAX_HEALTH = 100;
+        private const int HEALING_AMOUNT = 5;
+        private const float HEALING_INTERVAL = 0.5f;
+        private const float HEALING_DURATION = 3.0f;
+
+        #endregion
+
+
+        #region Properties
+
+        private int Health
+        {
+            get => _health;
+            set
+            {
+                _health = Mathf.Clamp(value, 0, MAX_HEALTH);
+                if (_healthBar)
+                    _healthBar.value = _health;
+                if (_healthText)
+                    _healthText.text = _health.ToString();
+            }
+        }
+
+        #endregion
+
+
+        #region UnityMethods
+
+        void Start()
+        {
+            ResetHealth();
+            _startHealingButton?.onClick.AddListener(ReceiveHealing);
+            _resetHealthButton?.onClick.AddListener(ResetHealth);
+        }
+
+        private void OnDestroy()
+        {
+            _startHealingButton?.onClick.RemoveAllListeners();
+            _resetHealthButton?.onClick.RemoveAllListeners();
+        }
+
+        #endregion
+
+
+        #region Methods
+
+        public void ReceiveHealing()
+        {
+            if (_healing != null && !_canRenewHealing)
+                return;
+                
+            if (_healing != null)
+                StopCoroutine(_healing);
+
+            _healing = StartCoroutine(Healing());
+        }
+
+        public void ResetHealth()
+        {
+            Health = 1;
+        }
+
+        IEnumerator Healing()
+        {
+            var duration = 0f;
+
+            while (duration < HEALING_DURATION && Health < MAX_HEALTH)
+            {
+                yield return new WaitForSeconds(HEALING_INTERVAL);
+                duration += HEALING_INTERVAL;
+                Health += HEALING_AMOUNT;
+            }
+
+            _healing = null;
+        }
+
+        #endregion
+    }
+}
