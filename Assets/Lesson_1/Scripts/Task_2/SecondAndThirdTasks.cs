e21ecf7bf2c1646b1b267d1839b0b08090a15df8
Windigo The Green <windigotp@gmail.com>
2022-02-24 18:50:47 +0800

    Lesson_1 Task_3 is done

--- Assets/Lesson_1/Scripts/Task_2/SecondAndThirdTasks.cs
+++ Assets/Lesson_1/Scripts/Task_2/SecondAndThirdTasks.cs
@@ -1,23 +1,26 @@
-using System.Collections;
-using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.UI;
 
-namespace Lesson_1.Task_2
+namespace Lesson_1.Task_2_3
 {
-    public class SecondTask : MonoBehaviour
+    public class SecondAndThirdTasks : MonoBehaviour
     {
         #region Fields
 
         [SerializeField] private Button _startTaskButton;
         [SerializeField] private Button _cancelButton;
+        [SerializeField] private Button _startWhatFasterTaskButton;
+        [SerializeField] private Button _cancelWhatFasterButton;
         [Space]
         [SerializeField] private float _timeToWait = 1f;
         [SerializeField] private int _framesToWait = 60;
 
         private bool _areTasksRunning;
+        private bool _areWhatFasterTasksRunning;
+
+        private CancellationTokenSource _whatFasterCts;
 
         #endregion
 
@@ -26,12 +29,15 @@ namespace Lesson_1.Task_2
 
         void Start()
         {
-            _startTaskButton?.onClick.AddListener(PerformTasks);
+            _startTaskButton?.onClick.AddListener(PerformTasksAsync);
+            _startWhatFasterTaskButton?.onClick.AddListener(WhatTaskFasterAsync);
         }
 
         private void OnDestroy()
         {
             _startTaskButton?.onClick.RemoveAllListeners();
+            _startWhatFasterTaskButton?.onClick.RemoveAllListeners();
+            _whatFasterCts?.Dispose();
         }
 
         #endregion
@@ -39,7 +45,7 @@ namespace Lesson_1.Task_2
 
         #region Methods
 
-        private async void PerformTasks()
+        private async void PerformTasksAsync()
         {
             if (_areTasksRunning)
                 return;
@@ -61,6 +67,39 @@ namespace Lesson_1.Task_2
             }
         }
 
+        private async void WhatTaskFasterAsync()
+        {
+            if (_areWhatFasterTasksRunning)
+                return;
+
+            _whatFasterCts = new CancellationTokenSource();
+
+            Debug.Log("WhatFaster tasks started\n" +
+                "Task 1 - Time Based  |  Task 2 = Frame Based");
+
+            _areWhatFasterTasksRunning = true;
+            var ct = _whatFasterCts.Token;
+
+            Task task1 = TimeBasedTask(ct); 
+            Task task2 = FrameBasedTask(ct);
+            bool finishedTask = await WhatTaskFasterAsync(ct, task1, task2);
+
+            Debug.Log($"Task {(finishedTask ? 1 : 2)} was faster");
+
+            _areWhatFasterTasksRunning = false;
+
+            _whatFasterCts?.Dispose();
+        }
+
+        public async Task<bool> WhatTaskFasterAsync(CancellationToken ct, Task task1, Task task2)
+        {
+            
+            Task finishedTask = await Task.WhenAny(task1, task2);
+            _whatFasterCts.Cancel();
+
+            return finishedTask == task1;
+        }
+
         private async Task TimeBasedTask(CancellationToken ct)
         {
             var time = 0f;
@@ -78,6 +117,7 @@ namespace Lesson_1.Task_2
             }
 
             Debug.Log("Time Based Task is done");
+            return;
         }
 
         private async Task FrameBasedTask(CancellationToken ct)
@@ -97,6 +137,7 @@ namespace Lesson_1.Task_2
             }
 
             Debug.Log("Frame Based Task is done");
+            return;
         }
 
         #endregion
